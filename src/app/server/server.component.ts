56e39a85bdfa1ef9b42f84f80fa5a5ba05f04c00
Satish Yadav <satish.yadav@nagarro.com>
2022-07-02 21:56:49 +0530

    Alert assignment completed (Practice code)

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  // selector: 'app-selector', // element selector
+  // selector: '.app-server', // class selector
   // selector: '[app-server]', // attribute selector
-  selector: '.app-server', // class selector
-  template: '<h1>Satish Kumar Yadav</h1>',
+  selector: 'app-server', // element selector
+  templateUrl: './server.component.html',
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent implements OnInit {
