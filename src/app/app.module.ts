63287ba2a32a29e4d7dd02a8348336f8eea1c701
Satish Yadav <satish.yadav@nagarro.com>
2022-07-03 18:56:45 +0530

    Remove unwanted component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
-import { UserComponent } from './user/user.component';
-import { DisplayDetailsComponent } from './display-details/display-details.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ServerComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent,
-    UserComponent,
-    DisplayDetailsComponent,
-  ],
+  declarations: [AppComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
